54de0d3d880e8c87b570b2c7a4fbd00c04e2e133
trilokgit <71178423+trilokgit@users.noreply.github.com>
2022-06-12 09:44:58 +0530

    Updates

---
+++ precache-manifest.5c3385eee27b3c562810db0c47ef7267.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7bb8c4990f1abb2fadf3",
+    "url": "/newRepo/static/css/main.bc60e64f.chunk.css"
+  },
+  {
+    "revision": "7bb8c4990f1abb2fadf3",
+    "url": "/newRepo/static/js/main.7bb8c499.chunk.js"
+  },
+  {
+    "revision": "0968c6bc88b43608ac25",
+    "url": "/newRepo/static/js/1.0968c6bc.chunk.js"
+  },
+  {
+    "revision": "24132415383cf392f0fb",
+    "url": "/newRepo/static/js/runtime~main.24132415.js"
+  },
+  {
+    "revision": "f7abccfc6dc14f3571d8e77000b1ee13",
+    "url": "/newRepo/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/newRepo/precache-manifest.5c3385eee27b3c562810db0c47ef7267.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/newRepo/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
